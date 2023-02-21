a73ac835d6d4424d4ed46b1e4911b95b273eccde
Christian Dimitri <cdimitri@e-verse.com>
2023-02-21 19:49:52 +0100

    ux/ui improved

--- e-verse.Navisworks.SelectByRevitId.Plugin/Windows/SelectByIdWindow.xaml.cs
+++ e-verse.Navisworks.SelectByRevitId.Plugin/Windows/SelectByIdWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using EVerse.Navisworks.Plugin.Common;
+﻿using Autodesk.Navisworks.Api;
+using EVerse.Navisworks.Plugin.Common;
 using EVerse.Navisworks.SelectByRevitId.Plugin.Utils;
 using System;
 using System.Collections.Generic;
@@ -23,11 +24,18 @@ namespace EVerse.Navisworks.SelectByRevitId.Plugin
     /// </summary>
     public partial class SelectByIdWindow : Window
     {
-        private const string IMAGE_PATH = "Images\\RID_32.jpg";
+        //private const string IMAGE_PATH = "Images\\RID_32.jpg";
+        private const string NO_REVIT_MODEL_MESSAGE = "No revit model available";
+        private const string INSERT_ELEMENT_ID_MESSAGE = "Insert element revit ID";
+
         public SelectByIdWindow()
         {
             InitializeComponent();
+
             //LoadAddinImage();
+            if (!Tools.IsRevitModelLoaded())
+                OffOn(false, NO_REVIT_MODEL_MESSAGE, Colors.Red);
+            else OffOn(true, INSERT_ELEMENT_ID_MESSAGE, Colors.LightGray);
         }
 
         //private void LoadAddinImage()
@@ -37,7 +45,14 @@ namespace EVerse.Navisworks.SelectByRevitId.Plugin
         //    Uri uri = new Uri(fullPath);
         //    SlideUp_Image.Source = new BitmapImage(uri);
         //}
-
+        private void OffOn(bool toggle, string message, System.Windows.Media.Color? color)
+        {
+            applyButton.Foreground = new SolidColorBrush(Colors.White);
+            applyButton.IsEnabled = toggle;
+            textBox.IsHitTestVisible = toggle;
+            textBox.IsEnabled = toggle;
+            textBox.Text = message;
+        }
         private void okButton_Click(object sender, EventArgs e)
         {
             var s = Tools.splitString(textBox.Text);
@@ -51,6 +66,11 @@ namespace EVerse.Navisworks.SelectByRevitId.Plugin
         private void Border_MouseDown(object sender, MouseButtonEventArgs e)
         {
             this.DragMove();
+        }
+
+        private void onMouseEnter(object sender, MouseButtonEventArgs e)
+        {
+            textBox.Text = string.Empty;
         }
     }
 }
